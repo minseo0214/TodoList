66eb8b111aaf0c0c203d595cefa82b601633dd4f
minseo0214 <minseo>
2022-07-08 18:18:30 +0900

    import 오류 수정과 전체 코드 리뷰를 받았습니다.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,17 +3,97 @@ import './styles.css'
 
 interface Todo {
   id: number
-  text: string
+  content: string
   done: boolean
 }
 
 export default function App() {
-  const [text, setText] = React.useState('')
+  const [todoInput, setTodoInput] = React.useState('')
   const [todos, setTodos] = React.useState<Todo[]>([])
 
-  fetch('/todos')
-    .then((res) => res.json())
-    .then((data) => console.log(data.rows))
+  const refetch = React.useCallback(async () => {
+    const fetched = await fetch('/todos')
+    const data = await fetched.json()
+    setTodos(data.rows)
+  }, [])
 
-  return <div>bye</div>
+  React.useEffect(() => {
+    refetch()
+  }, [refetch])
+
+  const onChangeTextInput: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTodoInput(e.target.value)
+  }
+
+  const createTodo = async (text: string) => {
+    await fetch('/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text }),
+    })
+    refetch()
+  }
+
+  const updateTodo = async (id: number, done: boolean) => {
+    await fetch(`/todos/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ done }),
+    })
+    refetch()
+  }
+
+  const deleteTodo = async (id: number) => {
+    await fetch(`/todos/${id}`, {
+      method: 'DELETE',
+    })
+    refetch()
+  }
+
+  return (
+    <div className='container'>
+      {/*title*/}
+      <div className='title'>todo list 1</div>
+      {/*input*/}
+      <input
+        className='inputBox'
+        value={todoInput}
+        onChange={onChangeTextInput}
+        onKeyPress={(e) => {
+          if (e.code === 'Enter') {
+            createTodo(todoInput)
+            setTodoInput('')
+          }
+        }}
+      />
+      {/*todo list*/}
+      <div className='todoList'>
+        {todos.map((todo) => (
+          <div
+            key={todo.id}
+            className='todoBox'
+            style={{ backgroundColor: todo.done ? 'gray' : 'white' }}
+          >
+            <div
+              className='doneBox'
+              onClick={() => {
+                updateTodo(todo.id, !todo.done)
+              }}
+            />
+            <div className='textBox'>{todo.content}</div>
+            <div
+              className='doneBox'
+              onClick={() => {
+                deleteTodo(todo.id)
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,31 +1,57 @@
 import * as fs from 'fs'
-import * as Koa from 'koa'
-import * as Router from 'koa-router'
-import * as bodyparser from 'koa-bodyparser'
-import * as serve from 'koa-static'
+import Koa from 'koa'
+import Router from 'koa-router'
+import bodyparser from 'koa-bodyparser'
+import serve from 'koa-static'
 import { createPool, sql } from 'slonik'
 
 const app = new Koa()
 const router = new Router()
 
-const pool = createPool('postgresql://todo_user:todo_user@localhost/todos')
+const pool = createPool('postgresql://todo_user:todo_user@localhost/todos2')
 pool.connect(async (connection) => {
   await connection.query(sql`SELECT * FROM todo`)
 })
 
+//CRUD (create, read, update, delete)
 router.get('/', async (ctx) => {
   const html = await fs.promises.readFile('./src/client/index.html', 'utf-8')
   ctx.body = html
-  //   if (html) {
-  //     ctx.body = html
-  //   } else {
-  //     ctx.body = 'error'
-  //   }
 })
 
 router.get('/todos', async (ctx) => {
-  const todos = await pool.query(sql`SELECT * FROM todo`)
+  const todos = await pool.query(sql`SELECT * FROM todo ORDER BY id desc`)
   ctx.body = JSON.stringify(todos)
+})
+
+router.post('/todos', async (ctx) => {
+  const { text } = ctx.request.body
+  await pool.query(
+    sql`INSERT INTO todo (content, done) values (${text}, false)`
+  )
+  ctx.status = 200
+})
+
+//update로 했을 때 왜 crash가 나지?
+router.patch('/todos/:id', async (ctx) => {
+  const { id } = ctx.params
+  const { done } = ctx.request.body
+  if (!id) {
+    ctx.status = 400
+    return
+  }
+  await pool.query(sql`UPDATE todo SET done = ${done} WHERE id = ${id}`)
+  ctx.status = 200
+})
+
+router.delete('/todos/:id', async (ctx) => {
+  const { id } = ctx.params
+  if (!id) {
+    ctx.status = 400
+    return
+  }
+  await pool.query(sql`DELETE FROM todo where id=${id}`)
+  ctx.status = 200
 })
 
 //serve를 해줘야 build 파일을 찾음
