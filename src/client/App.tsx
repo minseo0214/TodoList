276065cfdbe2f188602c98bcceda8bb62dbb3a73
minseo0214 <minseo>
2022-07-08 12:36:39 +0900

    tsconfig 파일 추가

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -9,7 +9,11 @@ interface Todo {
 
 export default function App() {
   const [text, setText] = React.useState('')
-  const [todos, setTodos] = React.useState([])
+  const [todos, setTodos] = React.useState<Todo[]>([])
+
+  fetch('/todos')
+    .then((res) => res.json())
+    .then((data) => console.log(data.rows))
 
   return <div>bye</div>
 }
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -3,9 +3,15 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import bodyparser from 'koa-bodyparser'
 import serve from 'koa-static'
+import { createPool, sql } from 'slonik'
 
 const app = new Koa()
 const router = new Router()
+
+const pool = createPool('postgresql://todo_user:todo_user@localhost/todos')
+pool.connect(async (connection) => {
+  await connection.query(sql`SELECT * FROM todo`)
+})
 
 router.get('/', async (ctx) => {
   const html = await fs.promises.readFile('./src/client/index.html', 'utf-8')
@@ -15,6 +21,11 @@ router.get('/', async (ctx) => {
   //   } else {
   //     ctx.body = 'error'
   //   }
+})
+
+router.get('/todos', async (ctx) => {
+  const todos = await pool.query(sql`SELECT * FROM todo`)
+  ctx.body = JSON.stringify(todos)
 })
 
 //serve를 해줘야 build 파일을 찾음
